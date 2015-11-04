fa2b7779e4d35a4d13b502a4030b0c0614a91f45
Sleaverson <s.sleaverson@gmail.com>
2015-11-04 17:11:09 +0100

    Generate page looking good :D

--- angular/src/app/components/generator/generator.controller.js
+++ angular/src/app/components/generator/generator.controller.js
@@ -15,55 +15,9 @@
         vm.undoText = 'Undo';
         vm.addText = 'Add';
 
-
-
-        /* 
-         * We get json data from each provider.
-         * In order to avoid duplicate data we need 
-         * to put all the data into one combined array 
-         * that we can easily use and filter in our view
-         */
-
-/* Ignore this!
- * Wanted to make this work
- * witout a functioning back-end.
-
-        // For now we just add it to the scope
-        vm.fbData = Generator.getFacebook();
-
-        Generator.getFacebookImages().success(function(data) {
-            // For now we just add it to the scope
-            $scope.fbImages = data;
-        });
-
-        Generator.getLinkedIn().success(function(data) {
-            // For now we just add it to the scope
-            $scope.linData = data;
-        });
-
-        Generator.getLinkedInPositions().success(function(data) {
-            // For now we just add it to the scope
-            $scope.linPositions = data;
-        });
-
-        Generator.getGithub().success(function(data) {
-            // For now we just add it to the scope
-            $scope.gitData = data;
-        });
-
-        Generator.getGithubEmail().success(function(data) {
-            // For now we just add it to the scope
-            $scope.gitEmail = data;
-        });
-
-        Generator.getGithubRepos().success(function(data) {
-            // For now we just add it to the scope
-            $scope.gitRepos = data;
-        });
-*/
-        Generator.getAllData().success(function(data) {
-            // For now we just add it to the scope
-            vm.data = data;
+        // Get al the data
+        Generator.query(function(data) {
+            vm.data = data.data;
         });
     }
 })();
